e467a4b3eff9b2eb9593fcfba460e670c9225fdf
Eten-Ekata <noeladaidu@gmail.com>
2023-05-28 11:29:12 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter, Route, Routes, Redirect  } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
